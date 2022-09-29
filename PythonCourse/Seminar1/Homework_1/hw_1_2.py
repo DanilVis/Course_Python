dbd0faef0b4946906033445396ccc75991828a1b
Danil <visdanil97@gmail.com>
2022-09-30 01:35:26 +0700

    Add another solutions for 1_2, 1_5

--- PythonCourse/Seminar1/Homework_1/hw_1_2.py
+++ PythonCourse/Seminar1/Homework_1/hw_1_2.py
@@ -7,3 +7,16 @@ for x in range(2):
         for z in range(2):
             result = not (x or y or z) == (not x and not y and not z)
             print(x, y, z, result)
+# ______________________________________________________
+
+# Вариант 2 - без вывода всех значений
+# i = 0
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             if not (not (x or y or z) == (not x and not y and not z)):
+#                 i += 1
+# if i: # if i != 0:
+#     print('False')
+# else:
+#     print('All True')
--- PythonCourse/Seminar1/Homework_1/hw_1_5.py
+++ PythonCourse/Seminar1/Homework_1/hw_1_5.py
@@ -8,3 +8,17 @@ y_2 = float(input('Введите координату точки y2: '))
 
 points_distance = ((x_1 - x_2)**2 + (y_1 - y_2)**2) **0.5
 print(round(points_distance, 2))
+# _______________________________________________________________________
+
+# Вариант 2 - с использованием функции sqrt
+
+# from math import sqrt
+
+# ax = float(input('Enter the x coordinate of the point A: '))
+# ay = float(input('Enter the y coordinate of the point A: '))
+
+# bx = float(input('Enter the x coordinate of the point B: '))
+# by = float(input('Enter the y coordinate of the point B: '))
+
+# distance_2d = round(sqrt((bx - ax) ** 2 + (by - ay) ** 2), 2)
+# print(distance_2d)
