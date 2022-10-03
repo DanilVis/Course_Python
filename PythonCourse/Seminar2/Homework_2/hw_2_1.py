089560ff25b24a8cb1374feb7c6c8d80cf380be5
Danil <visdanil97@gmail.com>
2022-10-04 02:12:52 +0700

    task 2_4 + hw_2_1, 2_2

--- PythonCourse/Seminar2/Homework_2/hw_2_1.py
+++ PythonCourse/Seminar2/Homework_2/hw_2_1.py
@@ -4,7 +4,23 @@ number = input('Введите число: ')
 result = 0
 
 for i in number:
-    if i.isdigit() == False:
-       continue
-    result += int(i)
+    if i.isdigit():
+       result += int(i)
 print(result)
+#__________________________________________
+
+# Вариант 2 - математически
+number = float(input('Введите число: '))
+
+if number < 0:
+    number *= -1 # Убираем минус у отрицательных
+
+l = len(str(number)) - 1
+num = int(number * 10 ** l) # Помогает избавиться от дробной части
+sum_num = 0
+
+while num != 0:
+    sum_num += num % 10
+    num = num // 10
+
+print(sum_num)
--- PythonCourse/Seminar2/Homework_2/hw_2_2.py
+++ PythonCourse/Seminar2/Homework_2/hw_2_2.py
@@ -1,10 +1,29 @@
 # 2.Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 
+# n = int(input('Введите число: '))
+
+# for i in range(1, n + 1):
+#     result = 1
+#     while i > 1:
+#         result *= i
+#         i -= 1
+#     print(result, end = ' ')
+
+# Исправленно:
 n = int(input('Введите число: '))
+result = 1
 
 for i in range(1, n + 1):
-    result = 1
-    while i > 1:
-        result *= i
-        i -= 1
+    result *= i
     print(result, end = ' ')
+#__________________________________________
+
+# Вариант 2 - с добавлением в список (меньше операций)
+# n = int(input('Введите число: '))
+# prod = 1
+# result_list = []
+
+# for i in range(1, n + 1):
+#     prod *= i
+#     result_list.append(prod)
+# print(result_list)
