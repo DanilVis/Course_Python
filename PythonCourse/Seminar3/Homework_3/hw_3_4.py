d6ec30257729a7ce405aeeccf8a538e5981e09b6
Danil <visdanil97@gmail.com>
2022-10-04 18:12:33 +0700

    Task hw_3_4

---
+++ PythonCourse/Seminar3/Homework_3/hw_3_4.py
@@ -0,0 +1,11 @@
+# 4.Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+entered_num = int(input('Введите число: '))
+# print(format(entered_num, 'b'))
+
+result = ''
+while entered_num != 0:
+    result = str(entered_num % 2) + result
+    entered_num //= 2
+
+print(result)
