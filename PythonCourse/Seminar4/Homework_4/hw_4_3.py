849c0c0289b84afc917951b936243bcf633acd79
Danil <visdanil97@gmail.com>
2022-10-16 22:33:28 +0700

    task hw 4_3 edit

--- PythonCourse/Seminar4/Homework_4/hw_4_3.py
+++ PythonCourse/Seminar4/Homework_4/hw_4_3.py
@@ -1,11 +1,39 @@
 # Задайте последовательность чисел. Напишите программу, которая 
 # выведет список неповторяющихся элементов исходной последовательности.
 
+# Вариант 1 - через список
+# num_list = list(map(int, input('Введите числа через пробел: ').split()))
+
+# unique_nums = []
+# for i in num_list:
+#     if num_list.count(i) == 1:
+#         unique_nums.append(i)
+# print(unique_nums)
+
+# Вариант 2 - через словарь, более оптимально
+from collections import Counter
+
+
 num_list = list(map(int, input('Введите числа через пробел: ').split()))
 
-unique_nums = []
-for i in num_list:
-    if num_list.count(i) == 1:
-        unique_nums.append(i)
-print(unique_nums)
+# count_dict = {}
+# for i in num_list:
+#     if not i in count_dict:
+#         count_dict[i] = 0
+#     count_dict[i] += 1
+count_dict = Counter(num_list)
 
+# res_list = []
+# for i in count_dict:
+#     if count_dict[i] == 1:
+#         res_list.append(i)
+res_list = [i for i in count_dict if count_dict[i] == 1]
+print(res_list)
+
+# 3 Вариант
+from collections import Counter
+
+
+num_list = list(map(int, input('Введите числа через пробел: ').split()))
+output_list = [k for k, v in Counter(num_list).items() if v == 1]
+print(output_list)
