ea0bd62c67826fb39bad711b1aafa4331476697b
Danil <visdanil97@gmail.com>
2022-10-18 03:03:35 +0700

    hw 5_3, hw 5_2

--- PythonCourse/Seminar5/Homework_5/hw_5_2.py
+++ PythonCourse/Seminar5/Homework_5/hw_5_2.py
@@ -9,11 +9,26 @@
 from random import choice, randint
 
 
+def input_number():
+    num_string = input('Введите, сколько конфет хотите взять: ')
+    while not num_string.isdigit() or int(num_string) > 28 or int(num_string) < 1:
+            num_string = input('Неверное число, нужно взять от 1 до 28 конфет: ')
+    return num_string
+
+
+def make_move(player, num_candies):
+    print(f'Конфет осталось: {num_candies}')
+    print(player, end = '')
+    player_turn = input_number()
+    num_candies -= int(player_turn)
+    return num_candies
+
+
 candies = 2021
 print('На столе лежит 2021 конфета. За один ход можно забрать не более чем 28 конфет.\
     \nПобедит тот, кто сделает последний ход\n')
 
-game_mode = input('Укажите число игроков (2 - игроков против игрока, 1 - игрок против компютера): ')
+game_mode = input('Укажите число игроков (2 - игрок против игрока, 1 - игрок против компютера): ')
 while not game_mode.isdigit() or int(game_mode) > 2 or int(game_mode) < 1:
     game_mode = input('Введите 1 или 2: ')
 print('Игра началась')
@@ -21,17 +36,9 @@ print('Игра началась')
 # Игра с человеком
 if game_mode == '2':
     while candies > 0:
-        print(f'Конфет осталось: {candies}')
-        first_player_turn = input('1й игрок, введите, сколько конфет хотите взять: ')
-        while not first_player_turn.isdigit() or int(first_player_turn) > 28 or int(first_player_turn) < 1:
-            first_player_turn = input('Неверное число, нужно взять от 1 до 28 конфет: ')
-        candies -= int(first_player_turn)
+        candies = make_move('1й игрок. ', candies)
         if candies > 0:
-            print(f'Конфет осталось: {candies}')
-            second_player_turn = input('2й игрок, введите, сколько конфет хотите взять: ')
-            while not second_player_turn.isdigit() or int(second_player_turn) > 28 or int(second_player_turn) < 1:
-                second_player_turn = input('Неверное число, нужно взять от 1 до 28 конфет: ')
-            candies -= int(second_player_turn)
+            candies = make_move('2й игрок. ', candies)
             if candies <= 0:
                 print('Второй игрок победил')
         else:
@@ -45,15 +52,11 @@ if game_mode == '1':
             print('Компьютер начинает первым')
             turn = 1
         else:
-            print(f'Конфет осталось: {candies}')
-            first_player_turn = input('Введите, сколько конфет хотите взять: ')
-            while not first_player_turn.isdigit() or int(first_player_turn) > 28 or int(first_player_turn) < 1:
-                first_player_turn = input('Неверное число, нужно взять от 1 до 28 конфет: ')
-            candies -= int(first_player_turn)
+            candies = make_move('', candies)
         if candies > 0:
             print(f'Конфет осталось: {candies}')
             computer_turn = candies % 29
-            if not computer_turn: # если игрок изначально взял такое кол-во конфет, что выиграет, если не ошибется
+            if not computer_turn: # когда игрок изначально взял такое кол-во конфет, что выиграет, если не ошибется
                 computer_turn = randint(1, 28)
             print(f'Компьютер взял {computer_turn} конфет')
             candies -= int(computer_turn)
---
+++ PythonCourse/Seminar5/Homework_5/hw_5_3.py
@@ -0,0 +1,67 @@
+# 3.Создайте программу для игры в "Крестики-нолики".
+
+def print_field(field_l):
+    for i in field_l:
+        print('|', end = '')
+        for j in i:
+            print(j, end = '|')
+        print()
+
+
+def valid_num(num_str):
+    valid = ['1', '2', '3']
+    while (not num_str.isdigit) or (num_str[0] not in valid) or (num_str[1] not in valid):
+            num_str = input('Нужно указать две цифры от 1 до 3: ')
+    return num_str
+
+
+def valid_cell(num_str, field_l):
+    a = int(num_str[0])
+    b = int(num_str[1])
+    while field_l[a - 1][b - 1] != '_':
+        num_str = valid_num(input('Клетка уже занята, введите новое значение: '))
+        a = int(num_str[0])
+        b = int(num_str[1])
+    return [a, b]
+
+
+def check_win(win_char):
+    for i in range(3):
+        if field[i][0] == field[i][1] == field[i][2] == win_char:
+            return True
+        if field[0][i] == field[1][i] == field[2][i] == win_char:
+            return True
+    if field[0][0] == field[1][1] == field[2][2] == win_char:
+        return True
+    return False
+
+
+def check_tie(field_l):
+    for i in field_l:
+        if '_' in i:
+                return False
+    return True
+
+
+field = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+print_field(field)
+
+dot_char1 = 'X'
+dot_char2 = 'O'
+while True:
+    print(f'Ходит {dot_char1}.')
+    move = valid_num(input('Введите номер строчки и столбца без пробела: '))
+    x, y = valid_cell(move, field)
+    field[x - 1][y - 1] = dot_char1
+    print_field(field)
+    if check_win(dot_char1):
+        print('Вы победили')
+        break
+    if check_tie(field):
+        print('У вас ничья')
+        break
+    dot_char1, dot_char2 = dot_char2, dot_char1
+
+
+
+    
